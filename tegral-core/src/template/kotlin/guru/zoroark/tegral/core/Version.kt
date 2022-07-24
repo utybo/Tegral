e3fba0ee802247ad66311f3db908d85e296ca5a0
Zoroark <utybodev@gmail.com>
2022-07-24 18:30:43 +0200

    Add version field in tegral-core

---
+++ tegral-core/src/template/kotlin/guru/zoroark/tegral/core/Version.kt
@@ -0,0 +1,5 @@
+package guru.zoroark.tegral.core
+
+// WARNING: THIS FILE CAN BE GENERATED! MAKE SURE YOU ARE EDITING THE VERSION IN THE TEMPLATE DIRECTORY!
+
+val tegralVersion: String = "${version}"
