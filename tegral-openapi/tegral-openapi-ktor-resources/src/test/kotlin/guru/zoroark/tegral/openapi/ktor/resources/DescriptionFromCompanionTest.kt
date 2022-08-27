ffbfa3a3e5a0febd0d52c0375d7b57c1d6b9df02
utybo <utybodev@gmail.com>
2022-08-27 22:16:02 +0200

    Update test and fix remaining check issues

--- tegral-openapi/tegral-openapi-ktor-resources/src/test/kotlin/guru/zoroark/tegral/openapi/ktor/resources/DescriptionFromCompanionTest.kt
+++ tegral-openapi/tegral-openapi-ktor-resources/src/test/kotlin/guru/zoroark/tegral/openapi/ktor/resources/DescriptionFromCompanionTest.kt
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package guru.zoroark.tegral.openapi.ktor.resources
 
 import guru.zoroark.tegral.openapi.dsl.OperationBuilder
@@ -5,6 +19,7 @@ import guru.zoroark.tegral.openapi.dsl.OperationDsl
 import io.mockk.mockk
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 @Suppress("UtilityClassWithPublicConstructor")
 class DescriptionFromCompanionTest {
@@ -42,15 +57,19 @@ class DescriptionFromCompanionTest {
 
     @Test
     fun `Companion object without interface`() {
+        // Should just be a no-op
         val description = descriptionFromCompanionObject<CompanionObjectWithoutInterface>()
-        TODO("What would be the appropriate reaction from the lib here?")
+        val result = OperationBuilder(mockk()).apply(description)
+        assertNull(result.description)
     }
 
     class NoCompanionObject
 
     @Test
     fun `No companion object`() {
+        // Should just be a no-op
         val description = descriptionFromCompanionObject<NoCompanionObject>()
-        TODO("What would be the appropriate reaction from the lib here?")
+        val result = OperationBuilder(mockk()).apply(description)
+        assertNull(result.description)
     }
 }
