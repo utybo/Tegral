8a2415aef87aec72d9ae4004cbb953c7e0c187a4
Zoroark <utybodev@gmail.com>
2022-12-26 21:32:30 +0100

    feat: use mainline jdks for toolchain

--- shell.nix
+++ shell.nix
@@ -3,12 +3,11 @@
 let
   unstable = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz) { };
   utybonp = import (fetchTarball https://github.com/utybo/nixpkgs/archive/refs/heads/gradle-toolchain-via-properties.tar.gz) { };
-  toolchains = [ unstable.jdk11 unstable.jdk17 ];
 in
 
 pkgs.mkShell {
   nativeBuildInputs = [
     unstable.nodePackages.pnpm
-    (utybonp.gradle.override { javaToolchains = [ unstable.jdk11 unstable.jdk17 ]; })
+    (utybonp.gradle.override { javaToolchains = [ pkgs.jdk11 pkgs.jdk17 ]; })
   ];
 }
