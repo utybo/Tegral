421dda88b7bb577425aa4615f4398c980114c67d
Zoroark <utybodev@gmail.com>
2022-12-26 21:57:49 +0100

    fix(docs): compilation works now

--- docs/src/routeobserver.js
+++ docs/src/routeobserver.js
@@ -1,24 +1,30 @@
 import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 import * as ackeeTracker from "ackee-tracker";
 
-let tracker = ackeeTracker.create("https://ackee.blastoise-1.zoroark.guru", {
-  detailed: true,
-  ignoreLocalhost: true,
-  ignoreOwnVisits: true,
-});
+const module = {};
 
-let siteId = "cea4d752-ff51-4b02-a9c4-f29765562938";
-let stop = null;
+if (ExecutionEnvironment.canUseDOM) {
+  let tracker = ackeeTracker.create("https://ackee.blastoise-1.zoroark.guru", {
+    detailed: true,
+    ignoreLocalhost: true,
+    ignoreOwnVisits: true,
+  });
 
-export function onRouteDidUpdate({ location, previousLocation }) {
-  if (stop != null) {
-    stop();
-  }
-  console.log(location);
-  if (
-    previousLocation == null ||
-    location.pathname !== previousLocation.pathname
-  ) {
-    stop = tracker.record(siteId).stop;
-  }
+  let siteId = "cea4d752-ff51-4b02-a9c4-f29765562938";
+  let stop = null;
+
+  module.onRouteDidUpdate = ({ location, previousLocation }) => {
+    if (location != undefined) {
+      if (stop != null) {
+        stop();
+      }
+      if (
+        previousLocation == null ||
+        location.pathname !== previousLocation.pathname
+      ) {
+        stop = tracker.record(siteId).stop;
+      }
+    }
+  };
 }
+export default module;
