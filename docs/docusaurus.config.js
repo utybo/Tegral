8f8d21f4c5c27a88d6832bee5528a8085cd66c07
Zoroark <utybodev@gmail.com>
2022-12-28 00:04:50 +0100

    docs: update mermaid to 2.2.0

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,54 +1,50 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const mdxMermaid = require('mdx-mermaid')
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Tegral',
-  tagline: 'Frameworks, libraries and DSLs for awesome Kotlin applications.',
-  url: 'https://tegral.zoroark.guru',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
-  favicon: 'img/tegral_logo.png',
+  title: "Tegral",
+  tagline: "Frameworks, libraries and DSLs for awesome Kotlin applications.",
+  url: "https://tegral.zoroark.guru",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "throw",
+  favicon: "img/tegral_logo.png",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'utybo', // Usually your GitHub org/user name.
-  projectName: 'tegral', // Usually your repo name.
+  organizationName: "utybo", // Usually your GitHub org/user name.
+  projectName: "tegral", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          remarkPlugins: [mdxMermaid],
-          editUrl:
-            'https://github.com/utybo/Tegral/tree/main/docs/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/utybo/Tegral/tree/main/docs/",
         },
         blog: {
           showReadingTime: true,
-          remarkPlugins: [mdxMermaid],
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           //editUrl:
           //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/'
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -58,66 +54,66 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Tegral',
+        title: "Tegral",
         logo: {
-          alt: 'Tegral Logo',
-          src: 'img/tegral_logo.svg',
+          alt: "Tegral Logo",
+          src: "img/tegral_logo.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'get-started/index',
-            position: 'left',
-            label: 'Get Started'
+            type: "doc",
+            docId: "get-started/index",
+            position: "left",
+            label: "Get Started",
           },
           {
-            type: 'doc',
-            docId: 'modules/index',
-            position: 'left',
-            label: 'Modules',
+            type: "doc",
+            docId: "modules/index",
+            position: "left",
+            label: "Modules",
           },
           {
-            type: 'doc',
-            docId: 'about/contributing',
-            position: 'left',
-            label: 'About'
+            type: "doc",
+            docId: "about/contributing",
+            position: "left",
+            label: "About",
           },
           {
-            href: 'pathname:///dokka',
-            label: 'API',
-            position: 'right'
+            href: "pathname:///dokka",
+            label: "API",
+            position: "right",
           },
           {
-            href: 'https://github.com/utybo/tegral/tree/main/CHANGELOG.md',
-            label: 'Changelog',
-            position: 'right'
+            href: "https://github.com/utybo/tegral/tree/main/CHANGELOG.md",
+            label: "Changelog",
+            position: "right",
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
+          { to: "/blog", label: "Blog", position: "left" },
           {
-            href: 'https://github.com/utybo/tegral',
-            label: 'GitHub',
-            position: 'right'
-          }
+            href: "https://github.com/utybo/tegral",
+            label: "GitHub",
+            position: "right",
+          },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Get Started & Tutorials',
-                to: '/docs/get-started'
+                label: "Get Started & Tutorials",
+                to: "/docs/get-started",
               },
               {
-                label: 'Modules',
-                to: '/docs/modules'
+                label: "Modules",
+                to: "/docs/modules",
               },
               {
-                label: 'API documentation (Dokka)',
-                to: 'pathname:///dokka'
-              }
+                label: "API documentation (Dokka)",
+                to: "pathname:///dokka",
+              },
             ],
           },
           // TODO add links
@@ -139,20 +135,20 @@ const config = {
             ],
           },*/
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/utybo/Tegral',
+                label: "GitHub",
+                href: "https://github.com/utybo/Tegral",
               },
               {
-                label: 'Changelog',
-                href: 'https://github.com/utybo/Tegral/tree/main/CHANGELOG.md'
-              }
+                label: "Changelog",
+                href: "https://github.com/utybo/Tegral/tree/main/CHANGELOG.md",
+              },
             ],
           },
         ],
@@ -161,15 +157,19 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['kotlin', 'groovy', 'toml', 'yaml'],
+        additionalLanguages: ["kotlin", "groovy", "toml", "yaml"],
       },
       algolia: {
-        appId: 'XKS5FWH0DJ',
-        apiKey: '6f8c788df7af2f39272bb5497cc19a8b',
-        indexName: 'tegral-zoroark'
-      }
+        appId: "XKS5FWH0DJ",
+        apiKey: "6f8c788df7af2f39272bb5497cc19a8b",
+        indexName: "tegral-zoroark",
+      },
     }),
   clientModules: [require.resolve("./src/routeobserver.js")],
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
 };
 
 module.exports = config;
